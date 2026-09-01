67d804e25028e68f98588b33c1664fc75e4308e4
Becauseiloveyo <1079458933@qq.com>
2026-09-01 20:24:27 +0800

    build: register Android test plugin for benchmarks

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,6 @@ buildscript {
 
 plugins {
     id("com.android.application") version "9.2.1" apply false
+    id("com.android.test") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
