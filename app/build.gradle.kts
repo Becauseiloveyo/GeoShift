f2f43bf5ae9a062ee85f8506a265ad1ba1e83336
Becauseiloveyo <1079458933@qq.com>
2026-09-01 16:33:31 +0800

    build: remove unused Compose tooling from debug APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
 
     testImplementation("junit:junit:4.13.2")
 }
