79be0d1a62b4a97cbafecd68f6512776064c0da7
Becauseiloveyo <1079458933@qq.com>
2026-09-01 16:26:27 +0800

    fix: use Material3 LocalContentColor

--- app/src/main/java/io/geoshift/app/ui/GeoShiftUiComponents.kt
+++ app/src/main/java/io/geoshift/app/ui/GeoShiftUiComponents.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -26,7 +27,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContentColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
