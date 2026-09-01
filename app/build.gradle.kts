f566784f7785d85127a06beea0dcec62df69706c
Becauseiloveyo <1079458933@qq.com>
2026-09-01 17:02:25 +0800

    build: bump GeoShift to v0.3-dev

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.geoshift.app"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0-dev"
+        versionCode = 3
+        versionName = "0.3.0-dev"
     }
 
     buildFeatures {
