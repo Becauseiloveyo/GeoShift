f9590cfdec199115cb0156f9df1dd095263c43ad
Becauseiloveyo <1079458933@qq.com>
2026-09-07 10:09:26 +0800

    fix: sanitize copied strict-session locations

--- app/src/main/java/io/geoshift/app/hooks/SystemLocationRewriter.kt
+++ app/src/main/java/io/geoshift/app/hooks/SystemLocationRewriter.kt
@@ -1,6 +1,7 @@
 package io.geoshift.app.hooks
 
 import android.location.Location
+import android.os.Build
 import io.geoshift.app.core.GeoProfile
 
 /**
@@ -16,6 +17,9 @@ internal object SystemLocationRewriter {
             latitude = profile.latitude
             longitude = profile.longitude
             if (!hasAccuracy()) accuracy = 5f
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                isMock = false
+            }
         }
     }
 
